0d07749379ebe80959331253cb44f06aa4b89d93
Adanna Mbanefo <graceandbesties@gmail.com>
2023-04-01 13:02:50 -0400

    my favourite food is pizza

--- get_smarter_today.py
+++ get_smarter_today.py
@@ -1,2 +1,4 @@
 print("I'm Ayesha Siddiqa")
 print("I love cookies")
+print("I'm Grace Mbanefo")
+print("I love cookies")
--- learning.py
+++ learning.py
@@ -1,1 +1,2 @@
 print("I loved learning about websites")
+print("I loved making things with the Arduino Kit")
